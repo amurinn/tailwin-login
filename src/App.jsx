c688790d089088aa26319458649613f671d6eede
amurin <amurin3d@gmail.com>
2022-08-17 22:34:30 +0200

    commit commig

--- src/App.jsx
+++ src/App.jsx
@@ -18,7 +18,7 @@ const App = () => {
 
   return (
     <div className="bg-dark-brown dark:bg-dark-blue w-screen h-screen flex duration-1000 transition">
-      <div className='bg-sun dark:bg-moon md:hidden w-[120px] aspect-square rounded-full absolute top-[162px] left-[312px] animate-moonAnimation cursor-pointer duration-1000 transition' onClick={changeTheme}></div>
+      <div className='bg-sun dark:bg-moon md:hidden w-[120px] aspect-square rounded-full absolute top-[110px] left-[251px] animate-moonAnimation cursor-pointer duration-1000 transition' onClick={changeTheme}></div>
       <Form />
       <div className="bg-sunset dark:bg-midnight flex-half bg-cover hidden md:block relative duration-1000 transition-all">
         <div className='bg-sun dark:bg-moon w-[120px] aspect-square rounded-full absolute top-[245px] left-[330px] animate-moonAnimation cursor-pointer duration-1000 transition' onClick={changeTheme}></div>
